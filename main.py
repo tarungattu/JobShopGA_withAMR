e5415d8952b9452c17db5152ce0167e7c5ea46e0
Tarun Gattu <162986890+tarungattu@users.noreply.github.com>
2024-09-29 09:35:47 +0530

    Initial commit
    added all files from previous repo

---
+++ main.py
@@ -0,0 +1,26 @@
+from JobShopScheduler import JobShopScheduler
+import benchmarks
+import distances
+
+def main():
+    machine_data = benchmarks.ft06['machine_data']
+    ptime_data = benchmarks.ft06['ptime_data']
+    scheduler1 = JobShopScheduler(6, 6, 2, 50, 0.7, 0.5, 100, machine_data, ptime_data)    
+    
+    scheduler1.set_distance_matrix(distances.six_machine_matrix)
+    scheduler1.display_schedule = 1
+    scheduler1.enable_travel_time = 1
+    chromosome1 = scheduler1.GeneticAlgorithm()
+    
+    scheduler1.reschedule(2, 3)
+    chromosome2 = scheduler1.GeneticAlgorithm()
+    
+    print('AMR MACHINE SEQUENCES')
+    print(chromosome1.amr_machine_sequences)
+    print(chromosome1.amr_ptime_sequences)
+    print(chromosome2.amr_machine_sequences)
+    print(chromosome2.amr_ptime_sequences)
+    
+    
+if __name__ == '__main__':
+    main()
---
+++ operation.py
@@ -0,0 +1,48 @@
+class Operation:
+    def __init__(self, job_number):
+        self.job_number = job_number
+        self.operation_number = 0
+        self.machine = None
+        self.Pj = None
+        self.start_time = 0
+        self.Cj = 0
+        self.travel_time = 0
+        
+    def __str__(self):
+        return str(self.job_number)
+        
+    def getCj(self):
+        self.Cj = self.start_time + self.Pj
+        
+        
+    # TURN ON SOURCE AND DISTANCE FOR USING TRAVEL TIME
+    def calculate_travel_time(self, amrs, jobs, distance_matrix, en_tt):
+        distance = 0
+        
+        velocity = amrs[jobs[self.job_number].amr_number].velocity
+        
+        if en_tt:
+            
+            source = self.machine
+            
+            if self.Pj == 0:
+                return 0
+            
+            # IF NEXT JOB PROCESSING TIME IS 0 PUT THIS JOB AS LAST JOB
+            if self.operation_number != len(jobs[self.job_number].operations) - 1:
+                if jobs[self.job_number].operations[self.operation_number + 1].Pj == 0:
+                    
+                    distance = distance_matrix[source][distance_matrix.shape[0]- 1] + distance_matrix[distance_matrix.shape[0] - 1][distance_matrix.shape[0] - 2]  
+                    
+                    return distance/velocity
+                    
+            if self.operation_number == len(jobs[self.job_number].operations) - 1:
+                
+                distance = distance_matrix[source][distance_matrix.shape[0]- 1] + distance_matrix[distance_matrix.shape[0] - 1][distance_matrix.shape[0] - 2]
+            
+            else:
+                dest = jobs[self.job_number].operations[self.operation_number + 1].machine
+                distance = distance_matrix[source][dest]
+                
+        return distance/velocity
+            
